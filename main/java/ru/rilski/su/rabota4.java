1f9e55af4b697205133fac0457ec696b2832c740
vasili <vasili2013@yandex.ru>
2018-10-20 18:29:00 +0300

    4 работа

---
+++ main/java/ru/rilski/su/rabota4.java
@@ -0,0 +1,12 @@
+package ru.rilski.su;
+
+public class rabota4 {
+    public static void main(String[] args) {
+        int a = 12;
+        int q = 18;
+        int f = 12 + 18;
+        if 10 <= f <= 20;{
+            System.out.println(true);
+        } System.out.println(false);
+    }
+}
