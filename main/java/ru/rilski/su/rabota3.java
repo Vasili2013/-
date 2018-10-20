ae82b33dbfb8e27071a70967847c70fd2e37cb79
vasili <vasili2013@yandex.ru>
2018-10-20 18:09:47 +0300

    3 задание

---
+++ main/java/ru/rilski/su/rabota3.java
@@ -0,0 +1,11 @@
+package ru.rilski.su;
+
+public class rabota3 {
+    public static int main(String[] args) {
+    int a = 48;
+    int b = 13;
+    int c = 12;
+    int d = 55;
+    return a * (b + (c / d));
+    }
+}
