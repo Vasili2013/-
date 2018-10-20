36803abc94daa06219c848b62897a05f9836a57c
vasili <vasili2013@yandex.ru>
2018-10-20 07:03:44 +0300

    коммент добавил

--- main/java/ru/rilski/su/rabottu.java
+++ main/java/ru/rilski/su/rabottu.java
@@ -1,5 +1,14 @@
 package ru.rilski.su;
 
 public class rabottu {
-    
+    public static void main(String[] args) {
+        byte a = 23 ;
+        short s = 222;
+        int d =4380;
+        long f = 9999999;
+        float q = 24; // число же при выводе и так дробное =)
+        double w = 34.45;
+        boolean aa = true, qq = false;
+        char Q = 'w';
+    }
 }
